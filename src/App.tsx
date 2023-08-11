05a7915dac2a800e798dbd00005e3fdc7d3baaaf
Leo Gharib <leonargharib@gmail.com>
2023-08-11 13:15:15 -0700

    prettier
    shadcn
    
    Inter font

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import { Button } from "components/Button";
 import "./App.css";
 import HelloWorld from "components/HelloWorld";
 
 function App() {
   return (
-    <div className="dark bg-background min-h-screen w-full">
+    <div className="dark bg-background min-h-screen w-full flex justify-center items-center gap-4 flex-col">
       <HelloWorld />
+      <Button>Click me!</Button>
     </div>
   );
 }
